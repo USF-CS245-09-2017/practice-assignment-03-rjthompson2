9946269b01409a8ec63b35f770b56a4739dad717
rjthompson2 <rjthompson2@dons.usfca.edu>
2019-02-15 10:32:51 -0800

    New Version

--- BinaryRecursiveSearch.java
+++ BinaryRecursiveSearch.java
@@ -1,23 +1,33 @@
 public class BinaryRecursiveSearch implements Practice03Search{
 	protected int start = 0;
-	protected int end = arr.length-1;
+	protected int end;
+	protected int count = 1;
+	protected int mid;
 
 	public int search(int [] arr, int target){
-		int mid = (start+end)/2;
+		mid = (start+end)/2;
+		if (count == 1){
+			end = arr.length-1;
+		}
+		//System.out.println(mid);
+		count++;
+		if (start> end){
+			//System.out.println(-1);
+			return -1;
+		}
 		if (arr[mid]== target){
+			//System.out.println(mid);
 			return mid;
 		}
 		else if (arr[mid]<target){
 			start = mid+1;
-			this.search(arr, target);
+			return this.search(arr, target);
 		}
 		else if (arr[mid]>target){
 			end = mid-1;
-			this.search(arr, target);
+			return this.search(arr, target);
 		}
-		else{
-			return -1;
-		}
+		return -1;
 	}
 
 	public String searchName(){
